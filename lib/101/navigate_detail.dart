667f3223c9463b7a9fd7c19d4818af937b00a76e
sennurcoban <ssennurcoban@gmail.com>
2022-11-14 11:05:49 +0300

    changes

--- lib/101/navigate_detail.dart
+++ lib/101/navigate_detail.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 
 class NavigateDetailLearn extends StatefulWidget {
-  const NavigateDetailLearn({super.key});
-
+  const NavigateDetailLearn({super.key, required this.isOkay});
+  final bool isOkay;
   @override
-  State<NavigateDetailLearn> createState() => _NavigateDetailLearnState();
+  State<NavigateDetailLearn> createState() => _NavigateLearnState();
 }
 
-class _NavigateDetailLearnState extends State<NavigateDetailLearn> {
+class _NavigateLearnState extends State<NavigateDetailLearn> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // ModalRoute.of(context).settings.arguments
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: ElevatedButton.icon(onPressed: (){
-          Navigator.of(context).pop(true);
+          Navigator.of(context).pop(!widget.isOkay);
         }, 
-        icon: Icon(Icons.check), 
-        label: Text('Onayla'),),
+        icon: Icon(Icons.check, color: widget.isOkay ? Colors.red : Colors.green,), 
+        label: widget.isOkay ? Text('Red') : Text('Onayla'),),
       ),
     );
   }
--- lib/101/navigation.dart
+++ lib/101/navigation.dart
@@ -11,18 +11,44 @@ class NavigationLearn extends StatefulWidget {
 }
 
 class _NavigationLearnState extends State<NavigationLearn> with NavigatorManager {
+  List<int> selectedItems = [];
+  void addSelected(int index,bool isAdd) {
+    setState(() {
+      isAdd ? selectedItems.add(index) : selectedItems.remove(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(itemBuilder: (context, index) {
-        return Placeholder(color: Colors.green,);
-      },),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return TextButton(
+            onPressed: () async {
+              final response = await navigateToWidgetNormal<bool>(context, 
+              NavigateDetailLearn(
+                isOkay: selectedItems.contains(index),
+              ));
+              if (response is bool) {
+                addSelected(index, response);
+              }
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Placeholder(
+                color: selectedItems.contains(index) ? Colors.green : Colors.red,
+              ),
+            ),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.navigation_rounded),
-        onPressed: () async {final response = await navigateToWidgetNormal<bool>(context, NavigateDetailLearn());
-        if (response ==true) {
+        onPressed: () async {
+          // Navigator.of(context).push(MaterialPageRoute(builder: (context){
+          //   return const ButtonLearn();
+          // },fullscreenDialog: true //aşagıdan yukarıya doğru açılır kapanır));
           
-        }
         },
       ),
     );
@@ -32,18 +58,26 @@ class _NavigationLearnState extends State<NavigationLearn> with NavigatorManager
 mixin NavigatorManager {
   void navigateToWidget(BuildContext context, Widget widget) {
     Navigator.of(context).push(
-            MaterialPageRoute(builder: (context){
-            return widget;
-          },
-          fullscreenDialog: true,
-          settings: RouteSettings(),),);
+      MaterialPageRoute(
+        builder: (context) {
+          return widget;
+        },
+        fullscreenDialog: true,
+        settings: RouteSettings(),
+      ),
+    );
   }
-  Future <T?>navigateToWidgetNormal<T> (BuildContext context, Widget widget) {
+
+  Future<T?> navigateToWidgetNormal<T>(BuildContext context, Widget widget) {
     return Navigator.of(context).push<T>(
-            MaterialPageRoute(builder: (context){
-            return widget;
-          },
-          fullscreenDialog: true,
-          settings: RouteSettings(),),);
+      MaterialPageRoute(
+        builder: (context) {
+          return widget;
+        },
+        settings: RouteSettings(
+
+        ),
+      ),
+    );
   }
 }
